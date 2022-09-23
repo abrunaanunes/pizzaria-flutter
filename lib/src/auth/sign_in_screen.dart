d16afcf4dc384c66cf761c0243cfed085d55b5c8
Bruna Nunes <bruna@websix.com.br>
2022-09-22 23:54:39 -0300

    Tela de cadastro

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gopizza/src/auth/components/custom_text_field.dart';
+import 'package:gopizza/src/auth/sign_up_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
@@ -24,7 +25,7 @@ class SignInScreen extends StatelessWidget {
                         style: TextStyle(fontSize: 40),
                         children: [
                           TextSpan(
-                            text: "Go",
+                            text: "UTF",
                             style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
@@ -139,7 +140,15 @@ class SignInScreen extends StatelessWidget {
                               ),
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (c) {
+                                  return SignUpScreen();
+                                },
+                              ),
+                            );
+                          },
                           child: const Text(
                             "Criar conta",
                             style: TextStyle(
